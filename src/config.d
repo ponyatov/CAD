7ad5ea1fb05b6199270981f3dd57944d615f5f36
Dmitry Ponyatov <dponyatov@gmail.com>
2024-01-29 06:08:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/config.d
@@ -0,0 +1,7 @@
+module config;
+
+/// @ref Window defaults
+enum Window : short {
+    Width = 640, ///< default width, raw pixels
+    Height = 480, ///< default height, raw pixels
+}
--- src/window.d
+++ src/window.d
@@ -1,17 +1,26 @@
 module window;
 
+import config;
+
 import std.stdio;
+import std.format;
+import std.string;
 
 // uses https://wiki.dlang.org/Simpledisplay.d
 
 class Window {
-    string title;
-    this(string title) {
+    string title; ///< window title
+    short width, height; ///< window size
+
+    this(string title, short width = config.Window.Width,
+            short height = config.Window.Height,) {
         this.title = title;
+        this.width = width;
+        this.height = height;
     }
 
-    override string toString() const @safe pure nothrow {
-        return "Window:" ~ title;
+    override string toString() const @safe pure {
+        return "Window:%s[%s@%s]".format(title, width, height);
     }
 
     void loop() {

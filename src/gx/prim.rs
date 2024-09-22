895966d4669c24c2e7b3a04bcabc2713f8d7155d
Dmitry Ponyatov <dponyatov@gmail.com>
2024-09-22 12:43:05 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/gx/prim.rs
+++ src/gx/prim.rs
@@ -1,13 +1,32 @@
 ///! graphical primitives
 use crate::gx::color;
 
+pub struct Layer {
+    z: u16,
+}
+
+/// User Coordinate System
+pub struct UCS {
+    x: i16,
+    y: i16,
+}
+
+enum Prim {
+    Line,
+    Poly,
+    Area,
+}
+
 /// classical TrueColor 24bit
 /// single point
 pub struct Point {
+    ucs: Option<UCS>,
     x: i16,
     y: i16,
     color: color::RGB,
     visible: bool,
+    layer: Option<Layer>,
+    parent: Option<Prim>,
 }
 
 /// stroke line
